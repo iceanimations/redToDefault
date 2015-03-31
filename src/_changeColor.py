4b4a0c797d1e4ad8fd64dba4961a6759df2066d8
Qurban <qurban_ali36@yahoo.com>
2015-03-31 14:37:21 +0500

    msg arg added

--- src/_changeColor.py
+++ src/_changeColor.py
@@ -3,13 +3,14 @@ import msgBox
 from PyQt4.QtGui import QApplication, QMessageBox
 import appUsageApp
 
-def change():
+def change(msg=True):
     nodes = nuke.selectedNodes('Read')
-    if not nodes:
-        msgBox.showMessage(QApplication.activeWindow(), title='Red To Default',
-                           msg='No Read node selected',
-                           icon=QMessageBox.Information)
-        return
+    if msg:
+        if not nodes:
+            msgBox.showMessage(QApplication.activeWindow(), title='Red To Default',
+                               msg='No Read node selected',
+                               icon=QMessageBox.Information)
+            return
     for node in nodes:
         node.knob('tile_color').setValue(0)
     
