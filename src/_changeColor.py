2672a17c42922171f61cb3cb52151b51d0f7ab0a
Talha Ahmed <talha.ahmed@gmail.com>
2019-09-16 20:10:48 +0500

    adding nuke11 support by employing Qt.py

--- src/_changeColor.py
+++ src/_changeColor.py
@@ -1,19 +1,22 @@
 import nuke
-import msgBox
-from PyQt4.QtGui import QApplication, QMessageBox
-import appUsageApp
+from Qt.QtWidgets import QApplication, QMessageBox
+
+from utilities import msgBox, appUsageApp
+
 
 def change(msg=True):
     nodes = nuke.selectedNodes('Read')
     if msg:
         if not nodes:
-            msgBox.showMessage(QApplication.activeWindow(), title='Red To Default',
-                               msg='No Read node selected',
-                               icon=QMessageBox.Information)
+            msgBox.showMessage(
+                    QApplication.activeWindow(),
+                    title='Red To Default',
+                    msg='No Read node selected',
+                    icon=QMessageBox.Information)
             return
     for node in nodes:
         node.knob('tile_color').setValue(0)
-    
+
     appUsageApp.updateDatabase('RedToDefault')
-    
+
     return True
